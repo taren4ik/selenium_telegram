4cc190fed9e8b432ca610ecd0cf093943f74a1b1
Dmitry Permyakov <taren4ik@mail.ru>
2023-04-15 23:55:56 +1000

    Добавил README.md  и ввод пароля.

---
+++ README.md
@@ -0,0 +1,42 @@
+#selenium_telegram
+
+
+###Клонировать репозиторий и перейти в него в командной строке:
+
+```
+bash
+git clone https://github.com/taren4ik/Fastapi_microblog
+cd Fastapi_microblog
+```
+
+###Cоздать и активировать виртуальное окружение:
+
+```
+bash
+python -m venv venv
+```
+
+####Для *nix-систем:
+```
+bash
+source venv/bin/activate
+```
+
+####Для windows-систем:
+```
+bash
+source venv/Scripts/activate
+```
+
+###Установить зависимости из файла requirements.txt:
+
+```
+bash
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+
+***Над проектом работали:***
+
+*Dmitry P. Github:https://github.com/taren4ik |
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from selenium import webdriver
+import os
 from time import sleep
 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+
 
 URL = "https://web.telegram.org/k/"
+CHANEL = "#@ERAanarchy"
 
+
+
+load_dotenv()
+
+
+PHONE = os.getenv('PHONE')
+PASSWORD = os.getenv('PASSWORD')
 
 def authorization():
     "Прохождение авторизации."
@@ -14,22 +25,42 @@ def authorization():
         value="/html/body/div[1]/div/div[2]/div[3]/div/div[2]/button[1]/div"
     )
     button.click()
+
     phone = driver.find_element(
         by=By.XPATH,
         value="/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[2]/div[1]"
     )
-    phone.send_keys("79990591101")
+    phone.send_keys(PHONE)
     sleep(3)
+
     next = driver.find_element(
         by=By.XPATH,
-        value="/html/body/div[1]/div/div[2]/div[2]/div/div[3]/button[1]/div"
+        value="/html/body/div[1]/div/div[2]/div[2]/div/div[3]/button[1]"
     )
     next.click()
+    sleep(5)
+
+    password = driver.find_element(
+        by=By.XPATH,
+        value="/html/body/div[1]/div/div[2]/div[5]/div/div[2]/div/input[2]"
+    )
+    sleep(5)
+    password.send_keys(PASSWORD)
+
+    pwd_next = driver.find_element(
+        by=By.XPATH,
+        value="/html/body/div[1]/div/div[2]/div[5]/div/div[2]/button/div"
+    )
     sleep(3)
+    pwd_next.click()
+
 
 
 def get_post():
-    sleep(3)
+    "Скриншоты постов."
+
+    driver.get(URL + "")
+    sleep(300)
 
 
 if __name__ == '__main__':
@@ -40,7 +71,7 @@ if __name__ == '__main__':
         options=chrome_options
     )
     driver.get(URL)
-    driver.implicitly_wait(15)
+    driver.implicitly_wait(20)
     authorization()
     get_post()
     driver.quit()
